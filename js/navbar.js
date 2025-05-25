0062638a3bd96667358f2200097adfb7c129a1a4
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2025-05-25 18:27:32 +0800

    testing landing page for logged in users

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,49 @@
-export async function navbar(container) {
-  container.innerHTML = `
-    <nav class="flex items-center justify-between bg-white py-4 px-6 lg:px-12 shadow-sm border-b border-gray-200">
-      <!-- Brand -->
-      <div class="text-gray-900 font-semibold text-xl tracking-wide">
-        BotoPH | 2025
-      </div>
+export function navbar(container) {
+    let isAuthenticated = false;
+    if(isAuthenticated) {
+        container.innerHTML = `
+            <nav class="flex items-center justify-between bg-white py-4 px-6 lg:px-12 shadow-sm border-b border-gray-200">
+                <!-- Brand -->
+                <div class="text-gray-900 font-semibold text-xl tracking-wide">
+                BotoPH | 2025
+                </div>
 
-      <!-- Menu links -->
-        <div id="menu" class="flex space-x-6">
-            <a href="../README.md" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap">
-                About Us
-            </a>
-            <a href="#" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap">
-                Signup
-            </a>
-            <a href="#" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap border border-gray-300 hover:border-gray-700">
-                Login
-            </a>
-        </div>
-    </nav>
-  `;
+                <!-- Menu links -->
+                <div id="menu" class="flex space-x-6">
+                    <a href="../README.md" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap">
+                        About Us
+                    </a>
+                    <a href="#" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap">
+                        Account
+                    </a>
+                    <a href="#" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap border border-gray-300 hover:border-gray-700">
+                        Logout
+                    </a>
+                </div>
+            </nav>
+        `;
+    } else {
+        container.innerHTML = `
+            <nav class="flex items-center justify-between bg-white py-4 px-6 lg:px-12 shadow-sm border-b border-gray-200">
+                <!-- Brand -->
+                <div class="text-gray-900 font-semibold text-xl tracking-wide">
+                BotoPH | 2025
+                </div>
+
+                <!-- Menu links -->
+                <div id="menu" class="flex space-x-6">
+                    <a href="../README.md" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap">
+                        About Us
+                    </a>
+                    <a href="#" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap">
+                        Signup
+                    </a>
+                    <a href="#" class="text-gray-700 hover:text-gray-900 transition font-medium px-3 py-2 rounded-md whitespace-nowrap border border-gray-300 hover:border-gray-700">
+                        Login
+                    </a>
+                </div>
+            </nav>
+        `;
+    }
+
 }
