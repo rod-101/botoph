302e1b9a7ac5f65a5914e9447b915038685a76e0
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2025-05-30 22:50:42 +0800

    add connection option for local and remote db

--- backend/dbConnection.php
+++ backend/dbConnection.php
@@ -1,15 +1,29 @@
 <?php
+
+// Localhost credentials
+$localHost = 'localhost';
+$localUsername = 'root'; // Change if different
+$localPassword = '';     // Change if your local MySQL has a password
+$localDbname = 'botoph'; // Change to your local database name
+
 $host = 'sql200.infinityfree.com';
 $username = 'if0_39059735';
 $password = '2G0o1rxhBKAmt1';
 $dbname = 'if0_39059735_botoph';
 
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
+//try local connection
+$conn = new mysql($localHost, $localUsername, $localPassword, $localDbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if($conn->connect_error) {
+    
+    // create remote connection
+    $conn = new mysqli($host, $username, $password, $dbname);
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+} else {
+    echo "Connect to local server";
 }
+
 $conn->set_charset("utf8mb4");
 ?>
